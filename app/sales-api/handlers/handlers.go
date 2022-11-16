fea5eb25a74895c3cb6e997eae7827645015dbb0
Pavel Lots <pavel418890@gmail.com>
2022-11-17 00:42:54 +0300

    added errors and panics handlers

--- app/sales-api/handlers/handlers.go
+++ app/sales-api/handlers/handlers.go
@@ -1,17 +1,18 @@
 package handlers
 
 import (
-	"github.com/pavel418890/service/business/mid"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/pavel418890/service/business/mid"
 
 	"github.com/pavel418890/service/foundation/web"
 )
 
 func API(build string, shutdown chan os.Signal, log *log.Logger) *web.App {
 
-	app := web.NewApp(shutdown, mid.Logger(log))
+	app := web.NewApp(shutdown, mid.Logger(log), mid.Errors(log), mid.Panics(log))
 
 	check := check{
 		log: log,
